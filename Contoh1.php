11f49447a57bcb57e7968bf7eeb98e82c49ec82e
destrirm03 <marwahdestri@gmail.com>
2022-09-28 20:08:53 +0700

    menampilkan biodata

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama saya Imam Nawawi
+             Saya tinggal di daerah ciputat
+             Olah raga yang saya sukai adalah 
+             Bulutangkis";
+    }
+    
+}
